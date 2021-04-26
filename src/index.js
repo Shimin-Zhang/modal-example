ccce6e13e2605c87ce4fb501990bbce120c48cd0
Shimin Zhang <john.shimin.zhang@gmail.com>
2021-04-25 23:42:24 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <div> Hello world </div>
+  </React.StrictMode>,
+  // The WP version with shortcode
+  // document.getElementById('calculator-root')
+  // The development version with root
+  document.getElementById('root')
+);
