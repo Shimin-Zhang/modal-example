1440bd87d03a9aff077f67c3ab2e2d66cee9d61d
Shimin Zhang <john.shimin.zhang@gmail.com>
2021-04-27 19:44:37 -0600

    finish the buttons

--- src/Container.js
+++ src/Container.js
@@ -2,16 +2,89 @@ import React, { useState } from 'react';
 import Modal from './component/modal';
 
 export default function Container() {
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modal1Open, setModal1Open] = useState(false);
+  const [modal2Open, setModal2Open] = useState(false);
 
-  const toggleModal= () => {
-    setModalOpen(!modalOpen);
+  const toggleModal1= () => {
+    setModal1Open(!modal1Open);
+  };
+
+  const toggleModal2= () => {
+    setModal2Open(!modal2Open);
+  };
+
+  const modal1Title = {
+    content: "A crocodile cannot stick its tongue out",
+    icon: {
+      type: 'ok',
+      color: 'green'
+    }
+  };
+
+  const toggleSuccess = () => {
+    const notice = document.getElementById('success-notice');
+    notice.classList.toggle('hidden');
+    setTimeout(() => {
+      notice.classList.toggle('hidden');
+    }, 3000);
+  }
+
+  const toggleFailure = () => {
+    const notice = document.getElementById('failure-notice');
+    notice.classList.toggle('hidden');
+    setTimeout(() => {
+      notice.classList.toggle('hidden');
+    }, 3000);
+  }
+
+  const modal1Actions = [
+    {
+      text: 'Success',
+      theme: 'green-dark',
+      action: toggleSuccess
+    },
+    {
+      text: 'Failure',
+      theme: 'red-dark',
+      action: toggleFailure
+    },
+    {
+      text: 'Cancel',
+      theme: 'indigo-light',
+    }
+  ];
+
+  const modal2Actions = [
+  ];
+
+  const modal2Title = {
+    content: "Some fungi create zombies, then control their minds",
+    icon: {
+      element: (<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+</svg>),
+      color: "red"
+    }
   };
 
   return (
     <>
-      <button onClick={toggleModal} className ="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <Modal open={modalOpen} setOpen={setModalOpen} />
+      <div id="success-notice" class="absolute top-0 px-8 py-2 rounded w-screen bg-green-300 hidden">
+        <span class="sm:text-3xl text-2xl font-medium title-font text-center text-green-700 mb-20">Example Success Notice
+        </span>
+      </div>
+      <div id="failure-notice" class="absolute top-0 px-8 py-2 rounded w-screen bg-red-300 hidden">
+        <span class="sm:text-3xl text-2xl font-medium title-font text-center text-red-700 mb-20">Example Failure Notice
+        </span>
+      </div>
+      <button onClick={toggleModal1} className ="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Toggle Modal 1</button>
+
+      <Modal key={1} open={modal1Open} setOpen={setModal1Open} title={modal1Title} actions={modal1Actions} />
+
+      <button onClick={toggleModal2} className ="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg mt-5">Toggle Modal 2</button>
+
+      <Modal key={2} open={modal2Open} setOpen={setModal2Open}
+        title={modal2Title} />
     </>
   )
 };
--- src/component/modal.js
+++ src/component/modal.js
@@ -2,13 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 export default function Modal(props) {
 
+  //TODO: Validation Props
+
   const {
-    open=false,
-    setOpen= () => {},
-    title = "A crocodile cannot stick its tongue out.",
-    titleIcon = "ok",
-    options = {},
-    children
+    open = false,
+    setOpen = () => {},
+    title = {
+      content: "",
+      icon: {
+        type: 'alert',
+        element: null,
+        color: 'red'
+      }
+    },
+    children,
+    actions = [ { text: 'Ok' }]
   } = props;
 
   const icons = {
@@ -17,7 +25,7 @@ export default function Modal(props) {
         <path fill-rule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
       </svg>),
     alert: (
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M10 1.944A11.954 11.954 0 012.166 5C2.056 5.649 2 6.319 2 7c0 5.225 3.34 9.67 8 11.317C14.66 16.67 18 12.225 18 7c0-.682-.057-1.35-.166-2.001A11.954 11.954 0 0110 1.944zM11 14a1 1 0 11-2 0 1 1 0 012 0zm0-7a1 1 0 10-2 0v3a1 1 0 102 0V7z" clipRule="evenodd" />
       </svg>)
   };
@@ -47,8 +55,8 @@ export default function Modal(props) {
     return  (
       <div className="w-full flex flex-grow flex-row-reverse z-20">
         <span className="cursor-pointer pt-4 pr-6 fixed" onClick={closeModal}>
-         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
           </svg>
         </span>
       </div>
@@ -56,14 +64,17 @@ export default function Modal(props) {
   };
 
   const TitleIcon = () => {
-      return (titleIcon && <div className="mr-2 ">{icons[titleIcon]}</div>)
+    return (
+      title?.icon &&
+      <div className={`mr-2 text-${(title?.icon?.color || "gray")}-500`}>       { title?.icon?.element || icons[title?.icon?.type] }
+      </div>);
   };
 
   const Title = () => {
     return (<div className="fixed flex flex-row full:w-3/4 full:min-h-0 full:max-h-72/100 xl:max-h-85/100 lg:w-8/12 2xl:w-1/2 bg-gray-50 t-0 pt-10 pb-3 px-12 text-xl bg-gray-50">
       <TitleIcon />
       <h2 className="text-gray-700 font-semibold text-left capitalize">
-        {title}
+        {title?.content}
       </h2>
     </div>);
   };
@@ -77,18 +88,42 @@ export default function Modal(props) {
     );
   };
 
+  const ActionButton = ({ action = {}}) => {
+    const actionFn = () => {
+      if (action?.action) {
+        action?.action()
+      };
+      closeModal();
+    };
+
+    const buttonThemes = {
+      'indigo-dark': "w-full text-white text-2xl rounded-md px-0 full:px-4 py-2 full:py-4 bg-indigo-500 hover:bg-indigo-600",
+      'indigo-light': "w-full text-indigo-500 text-2xl rounded-md px-0 full:px-4 py-2 full:py-4 bg-white border-2 border-indigo-500 hover:border-indigo-600 hover:text-indigo-600",
+      'green-dark': "w-full text-white text-2xl rounded-md px-0 full:px-4 py-2 full:py-4 bg-green-500 hover:bg-green-600",
+      'red-dark': "w-full text-white text-2xl rounded-md px-0 full:px-4 py-2 full:py-4 bg-red-500 hover:bg-red-600",
+      'default': "w-full text-white text-2xl rounded-md px-0 full:px-4 py-2 full:py-4 bg-indigo-500 hover:bg-indigo-600"
+    };
+
+    return (
+      <div className="mx-auto w-full full:w-1/2 px-0 full:px-4 my-4">
+        <button
+          onClick={ actionFn }
+          className= { buttonThemes[(action?.theme || 'default')]}
+        >
+        { action?.text || 'Submit' }
+        </button>
+      </div>
+    );
+  };
+
   const Actions = () => {
     return (
       <div className="px-12 mt-4 flex flex-row flex-wrap">
-        <div className="mx-auto w-full full:w-1/2 px-0 full:px-4 my-4">
-          <button className="w-full text-white text-2xl rounded-md px-0 full:px-4 py-2 full:py-4 bg-indigo-500 hover:bg-indigo-600"> Action </button>
-        </div>
-        <div className="mx-auto w-full full:w-1/2 px-0 full:px-4 my-4">
-          <button className="w-full text-white text-2xl rounded-md px-0 full:px-4 py-2 full:py-4 bg-indigo-500 hover:bg-indigo-600"> Action </button>
-        </div>
-        <div className="mx-auto w-full full:w-1/2 px-0 full:px-4 my-4">
-          <button className="w-full text-white text-2xl rounded-md px-0 full:px-4 py-2 full:py-4 bg-indigo-500 hover:bg-indigo-600"> Action </button>
-        </div>
+        {
+          actions.map((a, i) => {
+            return <ActionButton action={a} key={i} />
+          })
+        }
       </div>
     )
   };
@@ -104,12 +139,11 @@ export default function Modal(props) {
     </div>;
   };
 
-
   return (
     open &&
     (
       <>
-        <div id="modal-bg" onClick={closeModalBg} className="fixed top-0 h-full w-screen bg-gray-700 bg-opacity-75 top-0 flex flex-col justify-center">
+        <div id="modal-bg" onClick={closeModalBg} className="fixed top-0 h-full w-screen bg-gray-700 bg-opacity-75 top-0 flex flex-col justify-center transition-opacity duration-500">
           <ModalBody />
         </div>
       </>
